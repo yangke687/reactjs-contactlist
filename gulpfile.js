75792069b06e45825eec98ab7a34e7051694791b
Yang Ke <saint_yk@163.com>
2017-02-21 21:10:05 +0800

    add gulp-connect livereload plugin

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,39 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
+var connect = require('gulp-connect');
 
-gulp.task('browserify',function(){
+gulp.task('browserify', function() {
 	browserify('./src/js/main.js')
 		.transform('reactify')
 		.bundle()
 		.pipe(source('main.js'))
-		.pipe(gulp.dest('dist/js'));
+		.pipe(gulp.dest('dist/js'))
+		.pipe(connect.reload());
 });
 
-gulp.task('copy',function(){
+gulp.task('copy', function() {
 	gulp.src('src/index.html')
-		.pipe(gulp.dest('dist'));
+		.pipe(gulp.dest('dist'))
+		.pipe(connect.reload());
 	gulp.src('src/css/*.*')
-		.pipe(gulp.dest('dist/css'));
+		.pipe(gulp.dest('dist/css'))
+		.pipe(connect.reload());
 	gulp.src('src/js/vendors/*.*')
-		.pipe(gulp.dest('dist/js'));
+		.pipe(gulp.dest('dist/js'))
+		.pipe(connect.reload());
 });
 
-gulp.task('default',['browserify','copy'],function(){
-	return gulp.watch('src/**/*.*',['browserify','copy']);
+gulp.task('build', ['browserify', 'copy']);
+
+gulp.task('server', function() {
+	connect.server({
+		root: 'dist',
+		livereload: true,
+		port: 4321
+	});
 });
 
-
+gulp.task('default', ['browserify', 'copy', 'server'], function() {
+	return gulp.watch('src/**/*.*', ['browserify', 'copy']);
+});
