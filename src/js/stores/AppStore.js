57c19b1f7f98099ba56b187bb2d664ed652370aa
Yang Ke <saint_yk@163.com>
2017-03-07 22:19:34 +0800

    save contact to firebase

--- src/js/stores/AppStore.js
+++ src/js/stores/AppStore.js
@@ -2,33 +2,33 @@ var EventEmitter = require('events').EventEmitter;
 var assign = require('object-assign');
 var AppDispatcher = require('../dispatcher/AppDispatcher');
 var AppConstants = require('../constants/AppConstants');
-
+var AppAPI = require('../utils/appApi');
 var CHANGE_EVENT = 'change';
 
 var _contacts = [];
 
-var AppStore = assign({},EventEmitter.prototype,{
-	saveContact: function(contact){
+var AppStore = assign({}, EventEmitter.prototype, {
+	saveContact: function(contact) {
 		_contacts.push(contact);
 	},
-	getContacts: function(){
+	getContacts: function() {
 		return _contacts;
 	},
-	emitChange: function(){
+	emitChange: function() {
 		this.emit(CHANGE_EVENT);
 	},
-	addChangeListener: function(cb){
-		this.on(CHANGE_EVENT,cb);
+	addChangeListener: function(cb) {
+		this.on(CHANGE_EVENT, cb);
 	},
-	removeChangeListener: function(cb){
-		this.remove(CHANGE_EVENT,cb);
+	removeChangeListener: function(cb) {
+		this.remove(CHANGE_EVENT, cb);
 	}
 });
 
-AppDispatcher.register(function(payload){
+AppDispatcher.register(function(payload) {
 	var action = payload.action;
-	switch(action.actionType){
-		case AppConstants.SAVE_CONTACT: 
+	switch (action.actionType) {
+		case AppConstants.SAVE_CONTACT:
 			console.log('Saving Contact');
 			// store save
 			AppStore.saveContact(action.contact);
--- src/js/utils/appAPI.js
+++ src/js/utils/appAPI.js
@@ -1,6 +1,10 @@
+var firebase = require('firebase');
 
 module.exports = {
-	saveContact: function(){
-		
+	saveContact: function(contact) {
+		this.firebaseRef = new firebase('https://contactlist-2d3fd.firebaseio.com/');
+		this.firebaseRef.push({
+			contact: contact
+		});
 	}
 }
